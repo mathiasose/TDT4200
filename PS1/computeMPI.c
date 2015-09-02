23df0872f4c19cb08097b8dc3a950944465fb8cb
Mathias Ose <mathias.ose@gmail.com>
2015-09-02 18:11:10 +0200

    Implement parallel PS1 program.

--- PS1/computeMPI.c
+++ PS1/computeMPI.c
@@ -4,53 +4,71 @@
 #include <math.h>
 
 /*
-	A simple MPI example.
-	TODO:
-	1. Fill in the needed MPI code to make this run on any number of nodes.
-	2. The answer must match the original serial version.
-	3. Think of corner cases (valid but tricky values).
+   A simple MPI example.
+TODO:
+1. Fill in the needed MPI code to make this run on any number of nodes.
+2. The answer must match the original serial version.
+3. Think of corner cases (valid but tricky values).
 
-	Example input:
-	./simple 2 10000
+Example input:
+./simple 2 10000
 
-*/
+ */
 
 int main(int argc, char **argv) {
-	int rank, size;
+    int rank, size;
 
-	if (argc < 3) {
-		printf("This program requires two parameters:\n \
-the start and end specifying a range of positive integers in which \
-start is 2 or greater, and end is greater than start.\n");
-		exit(1);
-	}
+    if (argc < 3) {
+        printf("This program requires two parameters:\n \
+                the start and end specifying a range of positive integers in which \
+                start is 2 or greater, and end is greater than start.\n");
+        exit(1);
+    }
 
-	int start = atoi(argv[1]);
-	int stop = atoi(argv[2]);
+    int start = atoi(argv[1]);
+    int stop = atoi(argv[2]);
 
+    if(start < 2 || stop <= start){
+        printf("Start must be greater than 2 and the end must be larger than start.\n");
+        exit(1);
+    }
 
-	if(start < 2 || stop <= start){
-		printf("Start must be greater than 2 and the end must be larger than start.\n");
-		exit(1);
-	}
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	// TODO: Compute the local range, so that all the elements are accounted for.
+    // TODO: Compute the local range, so that all the elements are accounted for.
+    int t = (int) ((stop - start) / size);
+    start = start + rank * t;
+    stop = fmin(stop, start + t);
 
+    // Perform the computation
+    double sum = 0.0;
+    for (int i = start; i < stop ; i++) {
+        sum += 1.0/log(i);
+    }
 
-	// Perform the computation
-	double sum = 0.0;
-	for (int i = start; i < stop ; i++) {
-		sum += 1.0/log(i);
-	}
+    // Debug prints if needed
 
-	// Debug prints if needed
+    // TODO: Get the sum of each node's partial answer
+    // Use MPI_Send and MPI_Recv here
 
-	// TODO: Get the sum of each node's partial answer
-	// Use MPI_Send and MPI_Recv here
+    if (rank != 0) {
+        MPI_Send(&sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    } else {
+        for (int i = 1; i < size; i++) {
+            double temp;
+            MPI_Recv(&temp, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            sum += temp;
+        }
+    }
 
-	// TODO: Print the global sum once only
-	printf("The sum is: %f\n", sum);
+    // TODO: Print the global sum once only
+    if (rank == 0) {
+        printf("The sum is: %f\n", sum);
+    }
 
-	return 0;
+    MPI_Finalize();
+    return 0;
 }
 
--- PS1/computeSerial.c
+++ PS1/computeSerial.c
@@ -3,43 +3,43 @@
 #include <math.h>
 
 /*
-	A simple SERIAL example.
+   A simple SERIAL example.
 
-    Example input:
-    ./simple 2 10000
+   Example input:
+   ./simple 2 10000
 
-*/
+ */
 
 int main(int argc, char **argv) {
-	int rank, size;
+    int rank, size;
 
-	if (argc < 3) {
-		printf("This program requires two parameters:\n \
-the start and end specifying a range of positive integers in which \
-start is 2 or greater, and end is greater than start.\n");
-		exit(1);
-	}
+    if (argc < 3) {
+        printf("This program requires two parameters:\n \
+                the start and end specifying a range of positive integers in which \
+                start is 2 or greater, and end is greater than start.\n");
+        exit(1);
+    }
 
-	int start = atoi(argv[1]);
-	int stop = atoi(argv[2]);
+    int start = atoi(argv[1]);
+    int stop = atoi(argv[2]);
 
 
-	if(start < 2 || stop <= start){
-		printf("Start must be greater than 2 and the end must be larger than start.\n");
-		exit(1);
-	}
+    if(start < 2 || stop <= start){
+        printf("Start must be greater than 2 and the end must be larger than start.\n");
+        exit(1);
+    }
 
-	// Perform the computation
-	double sum = 0.0;
-	for (int i = start; i < stop ; i++) {
-		sum += 1.0/log(i);
-	}
+    // Perform the computation
+    double sum = 0.0;
+    for (int i = start; i < stop ; i++) {
+        sum += 1.0/log(i);
+    }
 
-	// Debug prints if needed
+    // Debug prints if needed
 
-	//Print the global sum once only
-	printf("The sum is: %f\n", sum);
+    //Print the global sum once only
+    printf("The sum is: %f\n", sum);
 
-	return 0;
+    return 0;
 }
 
